8d6f7414890bc382038929e0c498b6e385842eb6
William <wosteimer1@gmail.com>
2023-04-14 13:28:25 -0300

    feat(result.py): created Err class

--- src/result.py
+++ src/result.py
@@ -63,15 +63,51 @@ class Ok(Generic[_T, _E]):
 class Err(Generic[_T, _E]):
     __match_args__ = ("value",)
 
-    def __init__(self, value: _E) -> None:
-        self.__value = value
+    def __init__(self, Value: _E) -> None:
+        self.__value = Value
 
     @property
     def value(self) -> _E:
         return self.__value
 
     def unwrap(self) -> _T:
-        ...
+        raise InvalidUnwrapError()
+
+    def expect(self, message: str) -> _T:
+        raise InvalidUnwrapError(message)
+
+    def unwrap_or(self, default: _T) -> _T:
+        return default
+
+    def unwrap_or_else(self, default: Callable[[_E], _T]) -> _T:
+        return default(self.value)
+
+    def unwrap_err(self) -> _E:
+        return self.value
+
+    def expect_err(self, message: str) -> _E:
+        return self.value
+
+    def map(self, function: Callable[[_T], _U]) -> "Result[_U, _E]":
+        return Err(self.value)
+
+    def map_or(self, default: _U, function: Callable[[_T], _U]) -> _U:
+        return default
+
+    def map_or_else(
+        self, default: Callable[[_E], _U], function: Callable[[_T], _U]
+    ) -> _U:
+        return default(self.value)
+
+    def map_err(self, function: Callable[[_E], _F]) -> "Result[_T, _F]":
+        new_value = function(self.value)
+        return Err(new_value)
+
+    def is_ok(self) -> bool:
+        return False
+
+    def is_err(self) -> bool:
+        return True
 
 
 Result: TypeAlias = Ok[_T, _E] | Err[_T, _E]
--- tests/test_result.py
+++ tests/test_result.py
@@ -69,3 +69,70 @@ def test_is_ok_method_in_the_ok_state_must_return_true():
 def test_is_err_method_in_the_ok_state_must_return_false():
     result: Result[int, _] = Ok(1)
     assert not result.is_err()
+
+
+def test_the_unwrap_method_in_the_err_state_should_raise_an_exception():
+    result: Result[_, str] = Err("error")
+    with raises(InvalidUnwrapError):
+        result.unwrap()
+
+
+def test_the_expect_method_in_the_err_state_should_raise_an_exception_and_show_the_correct_message():
+    result: Result[_, str] = Err("error")
+    error_message = "error message"
+    with raises(InvalidUnwrapError) as error:
+        result.expect(error_message)
+        assert str(error) == error_message
+
+
+def test_the_unwrap_or_method_in_the_err_state_should_return_the_default_value():
+    result: Result[int, str] = Err("error")
+    default_value = 3
+    assert result.unwrap_or(default_value) == default_value
+
+
+def test_the_unwrap_or_else_method_in_the_err_state_should_correctly_apply_the_passed_default_function():
+    result: Result[_, str] = Err("error")
+    default_value = 3
+    assert result.unwrap_or_else(lambda _: default_value) == default_value
+
+
+def test_unwrap_err_method_in_err_state_return_the_correct_value():
+    result: Result[_, str] = Err("error")
+    assert result.unwrap_err() == "error"
+
+
+def test_expect_err_method_in_err_state_return_the_correct_value():
+    result: Result[_, str] = Err("error")
+    assert result.expect_err("error message") == "error"
+
+
+def test_map_method_in_the_err_state_should_return_an_err_object():
+    result: Result[_, str] = Err("error")
+    assert isinstance(result.map(lambda x: 1), Err)
+
+
+def test_map_or_method_in_the_err_state_should_return_the_default_value():
+    result: Result[int, str] = Err("error")
+    default_value = 3
+    assert result.map_or(default_value, lambda x: 1) == default_value
+
+
+def test_map_or_else_method_in_the_err_state_should_correctly_apply_the_passed_default_function():
+    result: Result[int, str] = Err("error")
+    assert result.map_or_else(lambda e: 3, lambda x: 1) == 3
+
+
+def test_map_err_in_the_err_state_should_correctly_apply_the_passed_function():
+    result: Result[int, str] = Err("error")
+    assert result.map_err(lambda e: "error_2").unwrap_err() == "error_2"
+
+
+def test_is_ok_method_in_the_err_state_must_return_false():
+    result: Result[_, str] = Err("error")
+    assert not result.is_ok()
+
+
+def test_is_err_method_in_the_err_state_must_return_true():
+    result: Result[_, str] = Err("error")
+    assert result.is_err()
